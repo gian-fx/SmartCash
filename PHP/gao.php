1bd744046189319886715c7d986da629c0698395
Pedro Mussio De Freitas <pedromfreitas200@gmail.com>
2025-11-11 15:56:01 -0300

    GAO

---
+++ PHP/gao.php
@@ -0,0 +1,96 @@
+<?php
+error_reporting(0);
+ini_set('display_errors', 0);
+
+$nada = "Dado não informado!";
+
+$exibir = $_SERVER['REQUEST_METHOD'] == 'POST' ? true : false;
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    if (isset($_POST['mc']) && !empty($_POST['mc'])) {
+
+        $mc = $_POST['mc'];
+    } else {
+
+        $mc = $nada;
+    }
+
+    if (isset($_POST['lucro']) && !empty($_POST['lucro'])) {
+
+        $lucro = $_POST['lucro'];
+    } else {
+
+        $lucro = $nada;
+    }
+
+    $gaoresult = $mc / $lucro;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/CSS/style.css">
+        <link rel="stylesheet" href="/CSS/mussio.css">
+        <title>SmartCash</title>
+    </head>
+    <body>
+
+        <div class="navbarbg">
+            <nav class="navbar">
+                <div class="logo">
+                    <a href="/index.html">SmartCash</a>
+                    <img src="/IMG/dollar.svg" alt="logo.">
+                </div>
+                <div class="itens">
+                    <ul>
+                        <li><a class="itensnav" href="/operacoes/ccc.html">CCC</a></li>
+                        <li><a class="itensnav" href="/operacoes/co.html">CO</a></li>
+                        <li><a class="itensnav" href="/operacoes/am.html">AM</a></li>
+                        <li><a class="itensnav" href="/operacoes/pe.html">PE</a></li>
+                        <li><a class="itensnav" href="/operacoes/mc.html">MC</a></li>
+                        <li><a class="itensnav" href="/operacoes/gao.html">GAO</a></li>
+                        <li><a class="itensnav" href="/operacoes/if.html">IF</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+
+        <div class="voltar">
+            <a href="/index.html"><img src="/IMG/btn_voltar.svg" alt=""></a>
+            <p>Grau de Alavancagem Operacional</p>
+        </div>
+
+        <div class="formgrid">
+            <div class="formcontainer">
+                <form method="POST" action="/PHP/gao.php">
+                    <div class="input">
+                        <input required type="number" placeholder="Adicione sua informação aqui" id="mc" name="mc">
+                        <label for="mc">Margem de Contribuição</label>
+                    </div>
+
+                    <div class="input">
+                        <label for="lucro">Lucro</label>
+                        <input required type="number" placeholder="Adicione sua informação aqui" id="lucro" name="lucro">
+                    </div>
+
+                    <div class="btn">
+                        <button type="submit">ENVIAR</button>
+                    </div>
+
+                </form>
+
+            </div>
+
+            <div class="resultado">
+                    <p>Seu Grau de Alavancagem Operacional é</p>
+                    <p class="resultadoccc"><?= $gaoresult ?></p>
+            </div>
+        </div>
+
+    </body>
+</html>
